099f0f9b2836349ed4e3a6a253c6300bc3aca368
Michelle Han <michellehan2007agt@gmail.com>
2023-08-02 13:15:32 -0700

    Initial Commit

---
+++ accompliSHARE1/accompliSHARE1App.swift
@@ -0,0 +1,17 @@
+//
+//  accompliSHARE1App.swift
+//  accompliSHARE1
+//
+//  Created by Michelle Han on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct accompliSHARE1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
