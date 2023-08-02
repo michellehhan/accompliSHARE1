d184f6dda275793db46d5cb7ca4d130bb229e473
Michelle Han <michellehan2007agt@gmail.com>
2023-08-02 14:38:23 -0700

    added buttons + profile

--- accompliSHARE1/ContentView.swift
+++ accompliSHARE1/ContentView.swift
@@ -13,37 +13,50 @@ struct ContentView: View {
     let people: [String] = ["Michelle Han", "Emily Markova", "Gnapika Birlangi", "Rachel Yoon", "Alicia Chiang", "Aayushi Garg", "Margot Robbie",  "Taylor Swift", "Dwayne Johnson", "Karlie Kloss"]
 
     var body: some View {
-        VStack {
-            Text("Find Friends")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.top, 90)
-                .padding(.bottom, 20)
+        NavigationView {
+            VStack {
+                Text("Find Friends")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 90)
+                    .padding(.bottom, 20)
 
-            TextField("Search...", text: $searchText)
-                .padding(7)
-                .background(Color.white)
-                .cornerRadius(8)
-                .padding(.horizontal, 10)
+                TextField("Search...", text: $searchText)
+                    .padding(7)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
 
-            ScrollView {
-                VStack(spacing: 10) {
-                    ForEach(people.filter { person in
-                        !searchText.isEmpty && person.lowercased().contains(searchText.lowercased())
-                    }, id: \.self) { person in
-                        Text(person)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding()
-                            .background(Color(red: 128/255, green: 185/255, blue: 235/255))
-                            .cornerRadius(8)
-                            .foregroundColor(.white)
+                ScrollView {
+                    VStack(spacing: 10) {
+                        ForEach(people.filter { person in
+                            !searchText.isEmpty && person.lowercased().contains(searchText.lowercased())
+                        }, id: \.self) { person in
+                            if person == "Karlie Kloss" {
+                                NavigationLink(destination: KarlieKlossView()) {
+                                    Text(person)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .padding()
+                                        .background(Color(red: 128/255, green: 185/255, blue: 235/255))
+                                        .cornerRadius(8)
+                                        .foregroundColor(.white)
+                                }
+                            } else {
+                                Text(person)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding()
+                                    .background(Color(red: 128/255, green: 185/255, blue: 235/255))
+                                    .cornerRadius(8)
+                                    .foregroundColor(.white)
+                            }
+                        }
                     }
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal)
             }
+            .background(Color(red: 240/255, green: 248/255, blue: 255/255))
+            .edgesIgnoringSafeArea(.all)
         }
-        .background(Color(red: 240/255, green: 248/255, blue: 255/255)) // Alice Blue
-        .edgesIgnoringSafeArea(.all) // To make the background color take up the full screen
     }
 }
 
@@ -52,4 +65,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
